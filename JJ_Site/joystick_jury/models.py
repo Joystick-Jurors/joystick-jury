7d921aa7afd384c082208bf1dc39eec0132912f6
slaugaus <slaugaus@gmail.com>
2024-03-20 23:31:35 -0600

    Update User Profile model

--- JJ_Site/joystick_jury/models.py
+++ JJ_Site/joystick_jury/models.py
@@ -6,8 +6,8 @@ def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename) 
 
 # Create your models here.
-class UserProfile(models.Model):  # TODO: Can probably customize the User class instead?
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to = user_directory_path)
 
 
