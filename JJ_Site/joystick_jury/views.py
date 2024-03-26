c4881f8f851ff185891565e5b3e319302dbfef14
Samhendricks105 <samhendricks360@gmail.com>
2024-03-26 17:16:23 -0600

    fixed some bugs

--- JJ_Site/joystick_jury/views.py
+++ JJ_Site/joystick_jury/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 
 def game_list(request):
     games = Game.objects.all()
-    return render(request, 'joy/game_list.html', {'games': games})
+    return render(request, 'JOYSTICK_JURY/game_list.html', {'games': games})
 
 def add_game(request):
     if request.method == 'POST':
@@ -14,7 +14,7 @@ def add_game(request):
         release_date = request.POST.get('release_date')
         Game.objects.create(title=title, publisher=publisher, release_date=release_date)
         return redirect('game_list')
-    return render(request,'joy/add_game.html')
+    return render(request,'JOYSTICK_JURY/add_game.html')
 
 def remove_game(request):
     if request.method == 'POST':
@@ -26,5 +26,5 @@ def remove_game(request):
             return redirect('game_list')
         except Game.DoesNotExist:
             error_message = f"No game found with title '{title}'."
-            return render(request, 'joy/remove_game.html', {'error_message': error_message})
-    return render(request, 'joy/remove_game.html')
+            return render(request, 'JOYSTICK_JURY/remove_game.html', {'error_message': error_message})
+    return render(request, 'JOYSTICK_JURY/remove_game.html')
