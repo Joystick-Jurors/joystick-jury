df2d4ca105b1124dd0e11767b8b2a1a239598fb1
joseph-robles-jr <rjosepha@byui.edu>
2024-03-28 17:22:17 -0600

    Revert "Merge branch 'main' of https://github.com/Joystick-Jurors/joystick-jury"
    This reverts commit b439435450e9030b5e56ca29766a82776fd949df, reversing
    changes made to c9911338e2fd7f192b8124b493713dc27f7c38c7.

--- JJ_Site/joystick_jury/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JoystickJuryConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "joystick_jury"
--- JJ_Site/joystick_jury/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.add_game, name ="add_game"),
-    path("games", views.game_list, name= "game_list"),
-    path('remove', views.remove_game, name='remove_game')
-]
